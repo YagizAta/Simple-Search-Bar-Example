cf922971bae820e7fe7d070216e2b70b433b8dc7
YagizAta <yagizata1@hotmail.com>
2021-05-04 15:00:31 +0300

    Initial Commit

---
+++ MyFruits/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyFruits
+//
+//  Created by Yağız Ata Özkan on 4.05.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyFruits/MyFruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFruitsApp.swift
+//  MyFruits
+//
+//  Created by Yağız Ata Özkan on 4.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct MyFruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
